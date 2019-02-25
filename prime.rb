65f8cfa2f69fc5e9826b5ec7d224c3155da415f9
Jerry <jvr572@Gmail.com>
2019-02-25 10:11:26 -0500

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,10 @@
-# Add  code here!
+def prime?(n)
+arr = []
+  if n <= 1
+    return false
+      # checks for negative numbers and ommits 1 as a prime
+  end
+  (2...n).each {|num| return false if n % num == 0 }
+  true
+
+end
